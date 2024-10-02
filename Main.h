c3301f8be599c171efc3986a5f42431b57617b70
mrvenumx <killthegame180408@gmail.com>
2024-10-03 00:30:09 +0700

    Patch: refactoring '#include' for 'Main.h'

--- Main.h
+++ Main.h
@@ -1,14 +1,12 @@
-#include <list>
-#include <vector>
-#include <string.h>
-#include <pthread.h>
-#include <thread>
-#include <cstring>
-#include <jni.h>
-#include <unistd.h>
-#include <fstream>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <list>
+#include <cstring>
+#include <thread>
+#include <unistd.h>
 #include <dlfcn.h>
+#include <jni.h>
 
 const char *libName = "libtarget.so";
 uintptr_t libBase;
